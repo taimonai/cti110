1fbd7511e1bb8b628cfc9328bd0936df38a08f51
taimonai <124944189+taimonai@users.noreply.github.com>
2023-05-02 21:49:44 -0400

    Add files via upload
    P4LAB1b

---
+++ P4LAB1b_FaulknerTainique.py
@@ -0,0 +1,49 @@
+# CTI-110
+
+# P4LAB1b - Initials
+
+# Tainique Faulkner
+
+# 5/2/2023
+
+#
+
+import turtle
+
+# Create a turtle object
+t = turtle.Turtle()
+
+# Set the color and pen size
+t.pencolor("pink")
+t.pensize(3)
+
+# Draw the T
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(25)
+t.right(180)
+t.forward(50)
+
+# Move to the right
+t.penup()
+t.goto(100, 0)
+t.pendown()
+t.penup
+t.right
+t.pendown
+
+# Draw the F
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(50)
+t.right(180)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+
+# Keep the window open
+turtle.mainloop()
